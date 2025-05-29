2d6cc25ed40bb568c31a5507d9d2d3e083247f18
Danielr-1998 <daniel.riano@utp.edu.co>
2025-05-29 10:35:11 -0500

    Fix Csv Converter format

--- src/main/java/com/backend/moviesapi/util/CsvUtils.java
+++ src/main/java/com/backend/moviesapi/util/CsvUtils.java
@@ -13,15 +13,18 @@ public class CsvUtils {
         List<Movie> movies = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(
                 Objects.requireNonNull(CsvUtils.class.getResourceAsStream(path))))) {
-            br.readLine(); // skip header
+            br.readLine(); // saltar encabezado
             String line;
             while ((line = br.readLine()) != null) {
-                String[] tokens = line.split(";");
+                String[] tokens = line.split(",");
+
                 Movie movie = new Movie(
-                        Integer.parseInt(tokens[0]),
-                        tokens[1], tokens[2], tokens[3],
-                        Integer.parseInt(tokens[4]),
-                        Integer.parseInt(tokens[5])
+                        Integer.parseInt(tokens[0].trim()),
+                        tokens[1].trim(),
+                        tokens[2].trim(),
+                        tokens[3].trim(),
+                        Integer.parseInt(tokens[4].trim()),
+                        Integer.parseInt(tokens[5].trim())
                 );
                 movies.add(movie);
             }
